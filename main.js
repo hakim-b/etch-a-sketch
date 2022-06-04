af7ab9b0c8d178eed811ee376fc0e4bd35bc05ea
hakim-b <hakimbouabdellah02@gmail.com>
2022-06-04 00:23:15 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+import "./style.css";
+
+const gridContainer = document.querySelector(".sketch-grid");
+const applyButton = document.getElementById('btnApply');
+const closeButton = document.getElementById('btnErase');
+const rangeMeter = document.getElementById('sizeInput');
+
+const createGrid = () => {
+
+}
+
+const reset = () => {
+    
+}
