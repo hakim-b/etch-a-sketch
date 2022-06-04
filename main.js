4558811f5e64c86826e0d7d37a59c6f7c98b8cfc
hakim-b <hakimbouabdellah02@gmail.com>
2022-06-04 15:43:38 -0400

    color picker

--- main.js
+++ main.js
@@ -4,6 +4,14 @@ const gridContainer = document.querySelector(".sketch-grid");
 const applyButton = document.getElementById('btnApply');
 const closeButton = document.getElementById('btnErase');
 const rangeMeter = document.getElementById('sizeInput');
+const colorPicker = document.getElementById('colorPicker');
+const boxDiv = document.querySelectorAll('.grid-box');
+
+boxDiv.forEach((square) => {
+    square.addEventListener('mouseover', () => {
+        square.style.backgroundColor = colorPicker.value;
+    });
+});
 
 const createGrid = () => {
 
