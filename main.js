54ae55351a16aa93a3236337bd68e8d485243b60
hakim-b <hakimbouabdellah02@gmail.com>
2022-06-04 21:58:14 -0400

    done

--- main.js
+++ main.js
@@ -1,22 +1,39 @@
 import "./style.css";
 
 const gridContainer = document.querySelector(".sketch-grid");
-const applyButton = document.getElementById('btnApply');
-const closeButton = document.getElementById('btnErase');
+const btnReset = document.querySelector(".btn-control");
 const rangeMeter = document.getElementById('sizeInput');
 const colorPicker = document.getElementById('colorPicker');
-const boxDiv = document.querySelectorAll('.grid-box');
+const gridSizeValue = document.getElementById('gridSize');
+let boxSize = parseInt(rangeMeter.value);
 
-boxDiv.forEach((square) => {
-    square.addEventListener('mouseover', () => {
-        square.style.backgroundColor = colorPicker.value;
-    });
+const setUpGrid = (size) => {
+    for (let i = 0; i < size * size; i++) {
+        gridContainer.style.gridTemplateRows = `repeat(${size}, auto)`;
+        gridContainer.style.gridTemplateColumns = `repeat(${size}, auto)`;
+
+        let square = document.createElement('div');
+        square.className = 'grid-box';
+        gridContainer.appendChild(square);
+
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = colorPicker.value;
+        });
+    }
+}
+
+document.body.addEventListener("load", setUpGrid(boxSize));
+
+rangeMeter.addEventListener("input", (event) => {
+    boxSize = event.target.value;
+    gridSizeValue.textContent = `${boxSize}x${boxSize}`;
 });
 
-const createGrid = () => {
+btnReset.addEventListener("click", () => {
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.lastChild);
+    }
 
-}
+    setUpGrid(boxSize);
+});
 
-const reset = () => {
-    
-}
